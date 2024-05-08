327094897e0f9b0de73fca9523e633f95edd82b8
Armando-lab <al066226@uacam.mx>
2024-05-08 01:26:36 -0600

    ss

--- application/views/Usuarios.php
+++ application/views/Usuarios.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />	
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	
