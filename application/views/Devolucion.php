a132b4ca9b0a1e03541a675ce711e3aae49ad84b
Armando-lab <al066226@uacam.mx>
2024-05-08 15:35:49 -0600

    ss

--- application/views/Devolucion.php
+++ application/views/Devolucion.php
@@ -507,11 +507,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								{ data: "estado" }
 							],
 							"createdRow": function (row, data) {
-								// Obtiene el color de la fila
-								var color = data.color;
+								// Obtener el tiempo transcurrido para el préstamo
+								var fechaPrestamo = new Date(data.fecha_prest);
+								var fechaActual = new Date();
+								var tiempoTranscurrido = Math.floor((fechaActual - fechaPrestamo) / (1000 * 60 * 60 * 24));
 
-								// Aplica el color de fondo a la fila
+								// Aplicar color según el tiempo transcurrido
+								var color;
+								if (tiempoTranscurrido <= 7) {
+									color = 'green'; // Verde si el préstamo es reciente
+								} else if (tiempoTranscurrido <= 30) {
+									color = 'yellow'; // Amarillo si el préstamo tiene menos de un mes
+								} else {
+									color = 'red'; // Rojo si el préstamo es antiguo
+								}
+
+								// Aplicar el color de fondo a la fila
 								$(row).css('background-color', color);
+							},
 							},
 							"footerCallback": function ( row, data, start, end, display ) {
 								var api = this.api(), data;
