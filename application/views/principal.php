914f77c3fcd0448064c91f94a3f9507b623d6310
Armando-lab <al066226@uacam.mx>
2024-05-08 00:51:57 -0600

    ss

--- application/views/principal.php
+++ application/views/principal.php
@@ -231,17 +231,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						function fillAreas(edificio, areaType) {
 							var areas = {
 								"B": {
-									"AULA": ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"],
-									"LABORATORIO": ["Sala Didactica", "Dibujo", "Lab. Aplicaciones", "Lab. Redes", "Eficiencia Energetica", "Posgrado 1", "Posgrado 2", "Posgrado 3"]
+									"AULA": ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
+									
 								},
 								"D": {
 									"AULA": ["42", "43", "44", "45", "46", "47"],
+									"Cubiculos": ["0"]
 								},
 								"E": {
 									"AULA": ["25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41"]
 								},
 								"F": {
-									"AULA": ["17"]
+									"AULA": ["17"],
+									"LABORATORIO": ["Sala Didactica", "Dibujo", "Lab. Aplicaciones", "Lab. Redes", "Eficiencia Energetica", "Posgrado 1", "Posgrado 2", "Posgrado 3"]
 								}
 								// Add more areas as needed
 							};
