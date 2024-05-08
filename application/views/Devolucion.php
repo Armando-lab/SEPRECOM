0e201a963e38f5d0d2800047138e656b45d344fa
Armando-lab <al066226@uacam.mx>
2024-05-08 02:35:13 -0600

    ss

--- application/views/Devolucion.php
+++ application/views/Devolucion.php
@@ -154,7 +154,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<label for="filtroEstado">Filtrar por Estado:</label>
 						<select id="filtroEstado">
 							<option value="">Todos</option>
-							<option value="Devuelto">Devuelto</option>
+							<option value="devuelto">Devuelto</option>
 							<option value="Prestado">Prestado</option>
 						</select>
 					</div>
@@ -571,7 +571,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						// Configura el filtro personalizado para la columna "Estado"
 						$('#filtroEstado').on('change', function() {
 							var estado = this.value;
-							table.column(6).search(estado).draw(); // Cambia 6 al índice de la columna "Estado"
+							table.column(7).search(<Estado).draw(); // Cambia 6 al índice de la columna "Estado"
 						});
 					});
 				</script>
