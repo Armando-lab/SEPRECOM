85984139126a24107b33fe31ae0726a9b507c2d9
Armando-lab <al066226@uacam.mx>
2024-05-19 21:42:23 -0600

    ss

--- application/models/Team_model.php
+++ application/models/Team_model.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Team_model extends CI_Controller {
+class Team extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
