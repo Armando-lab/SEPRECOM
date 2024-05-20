721584060c6bd005791dde34f06459b4b9b2ea37
Alfonso-Royedy <al067584@uacam.mx>
2024-05-20 01:10:23 -0600

    ss

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -66,8 +66,8 @@ class Dashboard_model extends CI_Model {
         $this->db->select('P.id_solicitud, C.nombre AS nombre_profesor');
         $this->db->from('prestamo P');
         $this->db->join('cliente C', 'P.profesor = C.matricula');
-        $this->db->where('DATE_ADD(P.fecha_prest, INTERVAL 3 DAY) <', 'CURDATE()', FALSE);
-    
+        $this->db->where('DATE_ADD(P.fecha_prest, INTERVAL -3 DAY) <', 'CURDATE()', FALSE);
+
         $query = $this->db->get();
     
         // Verificar si hay resultados
