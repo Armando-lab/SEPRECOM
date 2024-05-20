9ffc42b20791ea7f1f736a370029ff43ad77aa0c
Armando-lab <al066226@uacam.mx>
2024-05-19 21:59:11 -0600

    ss

--- application/controllers/Team.php
+++ application/controllers/Team.php
@@ -6,7 +6,7 @@ class Team extends CI_Controller {
 
     public function index() {
         $this->load->model('Team_model');
-        $data['team_members'] = $this->Team_model->get_team_members();
+        $data['team_members'] = $this->Team_model->index();
         $this->load->view('team_view', $data);
     }
 }
