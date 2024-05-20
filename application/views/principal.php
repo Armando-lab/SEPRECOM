b07662622dd2de7da1db96a5fecbbb12f467c5e9
Alfonso-Royedy <al067584@uacam.mx>
2024-05-20 04:26:47 -0600

    a

--- application/views/principal.php
+++ application/views/principal.php
@@ -122,7 +122,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 			<div style="text-align: center; cursor: pointer;" class="col-md-4" id="Vperfil">
-				<a style="text-decoration: none;background-color:#ecac54;" href='<?php echo base_url(); ?>index.php/devolucion' class="thumbnail">
+				<a style="text-decoration: none;background-color:#ecac54;"  class="thumbnail">
 					<h3 style="color:#FFFFFF">Total de Préstamos</h3>
 					<img style="width: 100px;" src="<?php echo base_url(); ?>application/views/imagenes/total.png" alt="...">
 					<h1 class="card-text" style="text-align: center; margin: 10px; color: #FFFFFF; --darkreader-inline-color: #bbb3a7;"><?php echo $totalPrestamos; ?></h1>
