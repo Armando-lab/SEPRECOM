00d27f05ea36f2da46de8ddc6399e7ba42e29271
Armando-lab <al066226@uacam.mx>
2024-05-23 14:26:14 -0600

    dsf

--- application/libraries/Rollbar_lib.php
+++ application/libraries/Rollbar_lib.php
@@ -1,29 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// Incluir los archivos necesarios de Rollbar
-require_once APPPATH . 'libraries/Rollbar/Rollbar.php';
-require_once APPPATH . 'libraries/Rollbar/DataBuilder.php';
-// Agrega más archivos necesarios de Rollbar aquí
 
 class Rollbar_lib {
     
     public function __construct($config = array()) {
-        $CI =& get_instance();
-        $CI->config->load('rollbar', TRUE);
-        $config = $CI->config->item('rollbar');
-
         if (!empty($config)) {
             $this->initialize($config);
         }
     }
     
     public function initialize($config = array()) {
-        Rollbar\Rollbar::init(array(
-            'access_token' => $config['access_token'],
-            'environment' => $config['environment'],
-            'root' => $config['root']
-        ));
+        Rollbar\Rollbar::init($config);
     }
 
     public function log($level, $message, $context = array()) {
